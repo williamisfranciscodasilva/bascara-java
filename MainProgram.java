d71275787136a1b213171a4b9478ad3fac642ab2
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-01-14 18:20:36 -0200

    Programa em Java que realiza a formula de bascara, depois que o usuário digita os valores de A, B e C

---
+++ MainProgram.java
@@ -0,0 +1,10 @@
+package bascara;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		formula f = new formula();
+		f.bascara();
+	}
+
+}
---
+++ formula.java
@@ -0,0 +1,25 @@
+package bascara;
+
+import javax.swing.JOptionPane;
+
+public class formula {
+	
+	public void bascara(){
+		
+		float a, b, c, delta, x1, x2;
+		
+		a = Float.parseFloat(JOptionPane.showInputDialog("Digite um valor para A: "));
+		b = Float.parseFloat(JOptionPane.showInputDialog("Digite um valor para B: "));
+		c = Float.parseFloat(JOptionPane.showInputDialog("Digite um valor para C: "));
+		
+		delta = (float) (Math.pow(b,2) -4 * a * c);
+
+		x1 = (float) (-b +(Math.sqrt(delta)))/(2*a);
+		x2 = (float) (-b -(Math.sqrt(delta)))/(2*a);
+		
+		
+		JOptionPane.showMessageDialog(null, "Para A= "+a+
+				"\nB="+b+"\nC="+c+"\nDelta = "+delta+
+				"\nX1 ="+x1+"\nX2 ="+x2);
+	}
+}
